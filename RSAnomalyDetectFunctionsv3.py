994b911e96dd2fb5f75c71edd1c4eb8d4a128417
IsaacSkyAlert <141506646+IsaacSkyAlert@users.noreply.github.com>
2024-07-24 09:48:41 -0600

    Update RSAnomalyDetectFunctionsv3.py

--- RSAnomalyDetectFunctionsv3.py
+++ RSAnomalyDetectFunctionsv3.py
@@ -30,7 +30,6 @@ constant_norm_lta = 1. / nlta
 complement_sta = 1 - constant_norm_sta 
 complement_lta = 1 - constant_norm_lta
 def STA_LTA(Signal):
-    import numpy as np
     ndat = len(Signal)
     sta = 0.
     lta = np.finfo(0.0).tiny 
@@ -39,37 +38,47 @@ def STA_LTA(Signal):
     for i in range(1, ndat):
         sta = constant_norm_sta * Signal[i] + complement_sta * sta
         lta = constant_norm_lta * Signal[i] + complement_lta * lta
-        Caracteristic_Function[i] = sta / lta
-    Caracteristic_Function[:clean_value] = 1 
-    return np.sqrt(Caracteristic_Function**2)
-
-thr_on = 1.40
-PEM = -1000
-def Anomaly_Did_Start(STA_LTA_Signal):
-    for i in range(clean_value, len(STA_LTA_Signal)):
-        if STA_LTA_Signal[i] > thr_on:
-            return i + PEM
-    return 0
+        Caracteristic_Function[i] = np.sqrt((sta / lta)**2) - 1
+    Caracteristic_Function[:clean_value] = 0
+    return Caracteristic_Function
     
-thr_off = 0.98
-PET = 1400
+thr_on = 0.80
+def Anomaly_Did_Start(STA_LTA_Signal):
+    for i in range(nlta, len(STA_LTA_Signal)):
+        if STA_LTA_Signal[i] > thr_on:
+            return i
+    return None
+ 
+thr_off = 0.001 
 def Anomaly_Did_End(STA_LTA_Signal, n_act):
     if n_act <= 0:
         return None
     n_start = n_act + 1
-    for j in range(n_start,len(STA_LTA_Signal)):
+    for j in range(n_start, len(STA_LTA_Signal)):
         if STA_LTA_Signal[j] < thr_off:
-            return j + PET
-    return 0
+            return j
+    return None
 
-def Get_Slice(Signal, i, j):
-    Slice = Signal[i:j]
-    return Slice
+def Get_Max_Amplitude(Signal):
+    max_amplitude = max(Signal**2)
+    return round(np.sqrt(max_amplitude),2)
 
-def PGA_Compute(Slice_ENN, Slice_ENE, Slice_ENZ):
-    square_ENN_max = np.max(Slice_ENN**2)
-    square_ENE_max = np.max(Slice_ENE**2)
-    square_ENZ_max = np.max(Slice_ENZ**2)
-    PGA = np.sqrt((square_ENN_max + square_ENE_max + square_ENZ_max)/3)
-    return PGA
+def Compute_PGA(a_ENZ,a_ENN,a_ENE):
+    PGA = (a_ENZ + a_ENN + a_ENE)/3
+    return round(PGA,2)
 
+def SkyAlert_Scale(gals):
+    if gals < 1:
+        return "Imperceptible"
+    elif 1 <= gals <= 5:
+        return "Débil"
+    elif 5.01 <= gals <= 21:
+        return "Leve"
+    elif 21.01 <= gals <= 35:
+        return "Moderado"
+    elif 35.01 <= gals <= 77:
+        return "Fuerte"
+    elif 77.01 <= gals <= 156:
+        return "Violento"
+    else:
+        return "Severo"
